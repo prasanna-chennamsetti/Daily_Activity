719a562ad7c0f304657034d9eb3cc42c60fabc70
Ch.Prasanna <170031562@kluniversity.in>
2021-04-18 15:27:21 +0530

    Lists and its operations commit from pycharm

---
+++ lists.py
@@ -0,0 +1,19 @@
+n=[1,2,3,4]
+print(n[2])
+x=["Str",[2,3,4],9]
+print(x[2])
+print(x[1][1])
+y=[2,4.5,5,8,9]
+print(y[2]+y[4])
+print(n[1]*6)
+z=["Word","Egg","Egg","one"]
+print("Egg" in z)
+print("sause" in z)
+print(len(z))
+z.append("Two")
+print(len(z))
+z.insert(3,"is")
+print(z[2]+" "+z[3]+" "+z[4])
+print(y.index(5))
+print(max(y))
+print(z.count("Egg"))
